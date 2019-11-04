21095867b021106c2878f671bd95c9a5e4feb251
Salman <salman.rahman515@gmail.com>
2019-11-04 03:34:05 -0500

    grpc: Client  - Sending proper request and receiving the proper response

--- src_2/client/batch_client.py
+++ src_2/client/batch_client.py
@@ -24,23 +24,92 @@ import grpc
 import batch_request_pb2
 import batch_request_pb2_grpc
 
+from time import sleep
+import json
 
-def run():
+
+def batch_request(request_json_par):
     # NOTE(gRPC Python Team): .close() is possible on a channel and should be
     # used in circumstances in which the with statement does not fit the needs
     # of the code.
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = batch_request_pb2_grpc.batchStub(channel)
         response = stub.getBatch(
-            batch_request_pb2.batch_info(rfwId='Salman', benchmarkType=11, workloadMetric=11, batchUnit=1, batchId=2,
-                                         batchSize=3))
-
-        # response = stub.getBatch(
-        #     batch_request_pb2.batch_info(rfwId='Salman', benchmarkType=))
-
-    print("Batch client received: " + response.response_batch)
+            batch_request_pb2.batch_info(rfwId=request_json_par["rfwId"],
+                                         benchmarkType=request_json_par["benchmarkType"],
+                                         workloadMetric=request_json_par["workloadMetric"],
+                                         batchUnit=request_json_par["batchUnit"],
+                                         batchId=request_json_par["batchId"], batchSize=request_json_par["batchSize"]))
+    return response.response_batch
 
 
 if __name__ == '__main__':
     logging.basicConfig()
-    run()
+
+while 1:
+
+    # Constraints
+    # batchId > 0
+
+    # ------ input -------------
+    # userInput = input("Please enter your selection from the options listed above: ")
+    # userInput = userInput.lower()
+    #
+    # if userInput == "2":
+    #     break
+    #
+    # rfwId = input("Please enter RFW ID: ")
+    # benchmarkType = input("Please enter Benchmark Type -> \n00 - DVD_Test, \n01 - DVD_Train, \n10 - NDBench_Test, \n11 - NDBench_Train: \nSelection -> ")
+    # workloadMetric = input("Please enter Workload Metric: ")
+    # batchUnit = input("Please enter Batch Unit: ")
+    # batchId = input("Please enter Batch ID: ")
+    # batchSize = input("Please enter Batch Size: ")
+    # ------ input -------------
+
+    # ------ delete -------------
+    rfwId = "salman"
+    benchmarkType = 1
+    workloadMetric = 1
+    batchUnit = 2
+    batchId = 1
+    batchSize = 3
+    # ------ delete -------------
+
+    request_json = {
+        "rfwId": rfwId,
+        "benchmarkType": benchmarkType,
+        "workloadMetric": workloadMetric,
+        "batchUnit": batchUnit,
+        "batchId": batchId,
+        "batchSize": batchSize
+    }
+
+    # ------ delete -------------
+    sleep(6)
+    print("woke up")
+    # ------ delete -------------
+
+    try:
+        response_batch = batch_request(request_json)
+
+        # https://www.w3schools.com/python/python_json.asp
+        batch_data_2_json = json.loads(response_batch)
+
+        batch_length = len(batch_data_2_json)
+
+        counter = 0
+        for x in batch_data_2_json:
+            counter = counter + 1
+            print(f"{counter} : {x}")
+
+    except:
+        print("Error: Could not call method in the server")
+
+    # try:
+    #     with open("client_data/client_request.json", 'w') as file:
+    #         json.dump(request_json, file)
+    #         # Save the JSON
+    #         # file.write(output)
+    #     # break
+    # except:
+    #     print("Error: Could not write to json file")
