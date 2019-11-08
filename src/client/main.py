a868f3a129c0eee44e5706dc39bbccac0625f0d2
Salman <salman.rahman515@gmail.com>
2019-11-08 04:31:10 -0500

    REST: Updated Client interface and server python version requirements

--- src/client/main.py
+++ src/client/main.py
@@ -16,24 +16,7 @@ batchUnit = -1  # number of samples
 batchId = -1  # 5th batch or 6th batch etc
 batchSize = -1  # how many batches to return
 
-# UI
-# userInput = ''
-
-# headers = {'Content-type': 'multipart/form-data'}
-# with open("client_data/client_request.json", "r") as read_file:
-#     data = json.load(read_file)
-# files = {'client_data': data}
-# r = requests.post(url, files=files, data=data, headers=headers)
-
-print(
-    f"Press {1} : To send a request workload data from the server\n"
-    f"Press {2} : To exit application\n"
-)
-
-# def is_input_numeric(var, check_str):
-#     while ~(var.isnumeric()):
-#         var = input(f"Please enter {check_str}: ")
-
+rfwId = 0
 
 while 1:
 
@@ -41,49 +24,60 @@ while 1:
     # batchId > 0
 
     # ------ input -------------
-    # userInput = input("Please enter your selection from the options listed above: ")
-    # userInput = userInput.lower()
-    #
-    # if userInput == "2":
-    #     break
-    #
+
+    print(
+        f"Press {1} : To request workload data from the server\n"
+        f"Press {2} : To exit application\n"
+        f"-----------------------------------------------------\n"
+    )
+
+    userInput = input("Please enter your selection from the options listed above: ")
+    userInput = userInput.lower()
+
+    if userInput == "2":
+        break
+
+    rfwId = rfwId + 1
+    print(f"rfwId: {rfwId}")
     # rfwId = input("Please enter RFW ID: ")
-    # benchmarkType = input("Please enter Benchmark Type -> \n1 - DVD_Test, \n2 - DVD_Train, \n3 - NDBench_Test, \n4 - NDBench_Train: \nSelection -> ")
-    # workloadMetric = input("Please enter Workload Metric -> \n1 - CPU, \n2 - NetworkIn, \n3 - NetworkOut, \n4 - Memory): \nSelection -> ")
-    # batchUnit = input("Please enter Batch Unit: ")
-    # batchId = input("Please enter Batch ID: ")
-    # batchSize = input("Please enter Batch Size: ")
+
+    benchmarkType = input(
+        "Please enter Benchmark Type -> \n1 - DVD_Test \n2 - DVD_Train \n3 - NDBench_Test \n4 - NDBench_Train \nSelection -> ")
+    workloadMetric = input(
+        "Please enter Workload Metric -> \n1 - CPU \n2 - NetworkIn \n3 - NetworkOut \n4 - Memory \nSelection -> ")
+    batchUnit = input("Please enter Batch Unit > -1 : ")
+    batchId = input("Please enter Batch ID > 0: ")
+    batchSize = input("Please enter Batch Size > -1: ")
     # ------ input -------------
 
-    # ------ delete -------------
-
-    rfwId = 0
-    benchmarkType = 1
-    workloadMetric = 4
-    batchUnit = 2
-    batchId = 1
-    batchSize = 3
-
-    # ------ delete -------------
+    # # ------ delete -------------
+    #
+    # rfwId = 0
+    # benchmarkType = 1
+    # workloadMetric = 4
+    # batchUnit = 2
+    # batchId = 1
+    # batchSize = 3
+    #
+    # # ------ delete -------------
+    # sleep(6)
+    # # print("woke up")
+    # # ------ delete -------------
+    #
+    # # ------ delete -------------
 
     request_json = {
-        "rfwId": rfwId,
-        "benchmarkType": benchmarkType,
-        "workloadMetric": workloadMetric,
-        "batchUnit": batchUnit,
-        "batchId": batchId,
-        "batchSize": batchSize
+        "rfwId": int(rfwId),
+        "benchmarkType": int(benchmarkType),
+        "workloadMetric": int(workloadMetric),
+        "batchUnit": int(batchUnit),
+        "batchId": int(batchId),
+        "batchSize": int(batchSize)
     }
-
-    # ------ delete -------------
-    sleep(6)
-    # print("woke up")
-    # ------ delete -------------
 
     try:
         r = requests.post(post_request_url, json=request_json)
 
-        # print(r.json())
         response_batch_data_json = r.json()
 
         response_rfwId = response_batch_data_json[0]
